6cf6aa9db1c48304e6fb413e80c92e92aaa4c312
alina.storme@student.ap.be <43607399+alintjax@users.noreply.github.com>
2020-01-20 09:34:04 +0100

    updated product_service.ts

--- Angular-Examen/src/app/service/product.service.ts
+++ Angular-Examen/src/app/service/product.service.ts
@@ -6,4 +6,26 @@ export class ProductService {
 
    constructor() {}
 
+    getAllProducts(): Product[] {
+      let products = [{
+            name:'Camera',
+            brand:'Nikon',
+            description:'Dit is een professionele camera voor afbeeldingen vast te leggen.',
+            price:165.50
+          },
+            {
+            name:'Iphone X',
+            brand:'Apple',
+            description:'Dit is een smartphone dat full face recognition bevat.',
+            price:799
+            }
+
+      ];
+      for (let i=0; i< products.length; i++){
+        let product = products[i];
+        let result = product;
+        products.push(result);
+      }
+      return products;
+    }
 }
