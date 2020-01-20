f82be24b3a01dfc0b09f7eae63efc1102b9eda03
alina.storme@student.ap.be <43607399+alintjax@users.noreply.github.com>
2020-01-20 11:53:47 +0100

    working sort!

--- Express-Examen/app/app.js
+++ Express-Examen/app/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var studentsRouter = require('./routes/students');
 
 var app = express();
 
@@ -19,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
+app.use('/users', usersRouter);
+app.use('/students',studentsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- Express-Examen/app/routes/students.js
+++ Express-Examen/app/routes/students.js
@@ -1,25 +1,37 @@
 var express = require('express');
+const app = express()
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 var db;
 
-MongoClient.connect('mongodb://localhost:27017', (err, database) => {
-    if(err) return console.log(err);
+MongoClient.connect('mongodb://localhost:27017/exam', (err, database) => {
+    if(err) return console.log(err)
     db = database.db('exam');
-})
-
-router.get('/',(req,res) => {
-    db.collection('students').find().toArray((err,result) => {
-        if(err) return console.log(err);
-       // console.log(result);
-        res.render('list.ejs',{students: result})
+    app.listen(process.env.PORT || 3000, () => {
+        console.log('Listening on port 3000')
     })
 })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+// Redirect to list
+app.get('/', (req, res) => {
+    res.redirect('/list')
+ })
+ 
+ // List all students
+ app.get('/list', (req, res) => {
+   db.collection('students').find().sort({naam: 1, geboortedatum: 1, studierichting: 1}).toArray((err, result) => {
+     if (err) return console.log(err)
+     res.render('list.ejs', { students: result })
+   })
+ })
 router.get('/add/', (req,res) => {
     res.render('add.ejs');
 })
 router.post('/add',(req,res) => {
-    db.collection('items').insertOne(req.body);
+    db.collection('students').insertOne(req.body);
     res.redirect('/students');
 })
 module.exports = router;
